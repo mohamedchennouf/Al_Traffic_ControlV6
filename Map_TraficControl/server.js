284551a8eda984ab9f8d81044c431432e1df039a
Mohamed Chennouf <mohamed.chennouf@hpe.com>
2018-01-24 10:52:03 +0100

    pop cars + map

--- Map_TraficControl/server.js
+++ Map_TraficControl/server.js
@@ -1,10 +1,16 @@
 var express = require('express');
  var app = express();
  app.get('/', function(req, res) {
-     res.sendFile('C:/Users/chennouf/Desktop/AL/map.html');
+     res.sendFile('C:/Users/chennouf/Desktop/AL_Traffic_ControlV6/Map_TraficControl/map.html');
  });
 app.get('/map', function(req, res) {
-     res.sendFile('C:/Users/chennouf/Desktop/AL/map.json');
+     res.sendFile('C:/Users/chennouf/Desktop/AL_Traffic_ControlV6/Map_TraficControl/map.json');
+ });
+app.get('/car0', function(req, res) {
+     res.sendFile('C:/Users/chennouf/Desktop/AL_Traffic_ControlV6/Map_TraficControl/image/car0.png');
+ });
+app.get('/car1', function(req, res) {
+     res.sendFile('C:/Users/chennouf/Desktop/AL_Traffic_ControlV6/Map_TraficControl/image/car1.png');
  });
  app.listen(8080); 
 
